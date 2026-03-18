5c57d41e0e161f0d9ace58e805560d5fd588b85c
IceCreamSandwhich <isaacosei4583@gmail.com>
2026-03-18 11:59:15 -0400

    Modifications

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,27 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
 	
-    bulmaSlider.attach();
+    var options = {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: false, 
+        autoplaySpeed: 5000,
+        pagination: true, // Dots at the bottom
+        navigation: true, // The Next/Prev arrows
+        
+        breakpoints: [
+            { changePoint: 480, slidesToShow: 1, slidesToScroll: 1 },
+            { changePoint: 640, slidesToShow: 1, slidesToScroll: 1 },
+            { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 }
+        ]
+    };
 
-})
+    // Initialize all carousels 
+    bulmaCarousel.attach('.carousel', options);
+    
+    // Initialize the slider extension
+    bulmaSlider.attach();
+});
